e57869a3cc2b07239daa6286d03e0e09f15fa236
mahnoor-hafeez <hafeezzmahnoor@gmail.com>
2024-12-08 10:58:28 +0500

    Add files via upload

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,23 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react({
+      jsxImportSource: '@emotion/react',
+      babel: {
+        plugins: ['@emotion/babel-plugin']
+      }
+    })
+  ],
+  server: {
+    port: 5173,
+    host: true,
+    strictPort: true
+  },
+  preview: {
+    port: 5173,
+    host: true,
+    strictPort: true
+  }
 })
